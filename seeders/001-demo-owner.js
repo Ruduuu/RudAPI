440c024795b678355e49815a55b0da23d84828d5
Rudresh <basrerudresh@gmail.com>
2026-07-27 13:28:17 +0530

    Update 001-demo-owner.js

--- seeders/001-demo-owner.js
+++ seeders/001-demo-owner.js
@@ -1,26 +1,25 @@
 'use strict';
 
 const bcrypt = require('bcryptjs');
-const { UUIDV4 } = require('sequelize');
 
 module.exports = {
-    async up(queryInterface) {
-        const now = new Date();
+  async up(queryInterface) {
+    const now = new Date();
 
-        await queryInterface.bulkInsert('Users', [{
-            id: require('sequelize').UUIDV4(),
-            name: 'Rud Owner',
-            email: 'owner@rudapi.dev',
-            password: await bcrypt.hash('owner123', 10),
-            role: 'owner',
-            createdAt: now,
-            updatedAt: now
-        }], {});
-    },
+    await queryInterface.bulkInsert('Users', [
+      {
+        // no id here
+        name: 'Rud Owner',
+        email: 'owner@rudapi.dev',
+        password: await bcrypt.hash('owner123', 10),
+        role: 'owner',
+        createdAt: now,
+        updatedAt: now
+      }
+    ], {});
+  },
 
-    async down(queryInterface) {
-        await queryInterface.bulkDelete(
-            'Users', { email: 'owner@rudapi.dev' }, {}
-        );
-    }
+  async down(queryInterface) {
+    await queryInterface.bulkDelete('Users', { email: 'owner@rudapi.dev' }, {});
+  }
 };
